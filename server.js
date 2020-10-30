5a9a8255510951141b0d5711db7656a5b258455a
Marcin Karbowniczyn <marcin.karbowniczyn@gmail.com>
2020-10-30 14:11:25 +0100

    Erased notes and todo from server.js

--- server.js
+++ server.js
@@ -33,29 +33,3 @@ process.on('SIGTERM', () => {
     console.log('Process terminated!');
   });
 });
-
-//////////////// TO DO W PROJEKCIE ///////////////////////
-/* 
-
-    1. Szybki front-end, dodanie remindera, zobaczenie swoich reminderów, możliwość usunięcia, edycji? (To w ramach rozbudowy projektu.)
-    2. Error handler serverowy, taki jak w natours.
-    3. Powtórzenie JSowego frontendu na przykładzie appki z 1 kursu.
-
-*/
-
-//////////////// NOTKI, PRZYPOMNIENIA, CO ZROBIĆ W TRAKCIE PROJEKTU? ///////////////////////
-/*
-    1. Obejrzeć film o API i restAPI.
-    4. Czym jest quicksort?
-    7. Powtórzyć slajdy z kursu Node, agr. pipeline, streams i events. Events nie wiem czy muszę.
-    8. Poczytać o util
-    14. Czym był Multer i multi-part form data
-    15. Jak działa indeksowanie, że np. kombinacja user i tour może być unique. Czy mogę to użyć u siebie?
-    16. UnhandledRejection i SIGTERM, co to było?
-    17. Czym właściwie są Promise   
-    19. Przypomnieć sobie ten debugger co używałem.
-    20. Co to jest branch w github?
-    21. Przypomnieć sobie funkcje find, findIndex i includes
-    22. Dokładnie poczytać o i++ i ++i i while loop
-    
-*/
